b36966d777af4b98d33bfb60e8edc7dde308a3f6
Red Skull <33503692+hydra007@users.noreply.github.com>
2018-03-10 16:05:29 -0500

    Update lib.rs

--- boot/lib.rs
+++ boot/lib.rs
@@ -17,8 +17,7 @@ pub extern fn rust_main() {
     // ATTENTION: we have a very small stack and no guard page
 
     vga_buffer::clear_screen();
-    println!("impOS")
-    printlt!("v0.0.1")
+    println!("impOS, v0.0.1")
 
     loop{}
 }
