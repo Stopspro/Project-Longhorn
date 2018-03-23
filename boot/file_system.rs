c1103bc00f842e4d3c1c0c587cc03250e2a020cb
Red Skull <33503692+hydra007@users.noreply.github.com>
2018-03-23 11:50:51 -0400

    Create file_system.rs

---
+++ boot/file_system.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+pub fn scan() { // runs a scan of all the files/packages installed.
+  loop {
+    let dir =
+    let paths = fs::read_dir(dir).unwrap();
+    for path in paths {
+      println!("File: {}", path.unwrap().path().display())
+  
+    println!("DIR: { }", dir);
+  }
+}
+
+pub fn virus() { // by default, the virus option should come disabled. By using the config though, it should be activated.
+  loop {
+    let paths = fs::read_dir(dir).unwrap();
+  }
+}
+
+pub fn branch() { // shows a basic illustration of the file "web"
+  println!("")
+}
+
+pub fn update() { // updates the file
+  
+}
