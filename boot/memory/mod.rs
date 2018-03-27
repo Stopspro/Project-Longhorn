a2bf17980fe9992d0011ddc7ce4a55f74d3f8288
Red Skull <33503692+hydra007@users.noreply.github.com>
2018-03-27 13:54:10 -0400

    Update mod.rs

--- boot/memory/mod.rs
+++ boot/memory/mod.rs
@@ -1,4 +1,5 @@
 pub use self::area_frame_allocator::AreaFrameAllocator;
+pub use self::paging::remap_the_kernel;
 use self::paging::PhysicalAddress;
 
 mod area_frame_allocator;
@@ -19,7 +20,37 @@ impl Frame {
     fn start_address(&self) -> PhysicalAddress {
         self.number * PAGE_SIZE
     }
+
+    fn clone(&self) -> Frame {
+        Frame { number: self.number }
+    }
+
+    fn range_inclusive(start: Frame, end: Frame) -> FrameIter {
+        FrameIter {
+            start: start,
+            end: end,
+        }
+    }
 }
+
+struct FrameIter {
+    start: Frame,
+    end: Frame,
+}
+
+impl Iterator for FrameIter {
+    type Item = Frame;
+
+    fn next(&mut self) -> Option<Frame> {
+        if self.start <= self.end {
+            let frame = self.start.clone();
+            self.start.number += 1;
+            Some(frame)
+        } else {
+            None
+        }
+    }
+ }
 
 pub trait FrameAllocator {
     fn allocate_frame(&mut self) -> Option<Frame>;
