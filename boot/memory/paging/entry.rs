51b6501d493c8df871b952d75af40c9b8b8b4cd4
Red Skull <33503692+hydra007@users.noreply.github.com>
2018-03-27 13:52:00 -0400

    Update entry.rs

--- boot/memory/paging/entry.rs
+++ boot/memory/paging/entry.rs
@@ -1,10 +1,10 @@
 // Most (if not all) of this code cam from Phil Opperman's wonderful OS guide. Thank you!
 use memory::Frame;
+use multiboot2::ElfSection;
 
 pub struct Entry(u64);
 
 impl Entry {
-
     pub fn is_unused(&self) -> bool {
         self.0 == 0
     }
@@ -45,5 +45,28 @@ bitflags! {
         const HUGE_PAGE =       1 << 7,
         const GLOBAL =          1 << 8,
         const NO_EXECUTE =      1 << 63,
+    }
+}
+
+
+impl EntryFlags {
+    pub fn from_elf_section_flags(section: &ElfSection) -> EntryFlags {
+        use multiboot2::{ELF_SECTION_ALLOCATED, ELF_SECTION_WRITABLE,
+            ELF_SECTION_EXECUTABLE};
+
+        let mut flags = EntryFlags::empty();
+
+        if section.flags().contains(ELF_SECTION_ALLOCATED) {
+            // section is loaded to memory
+            flags = flags | PRESENT;
+        }
+        if section.flags().contains(ELF_SECTION_WRITABLE) {
+            flags = flags | WRITABLE;
+        }
+        if !section.flags().contains(ELF_SECTION_EXECUTABLE) {
+            flags = flags | NO_EXECUTE;
+        }
+
+        flags
     }
 }
