6e01d670224a7f6a79e4526c911905bc0e109768
Red Skull <33503692+hydra007@users.noreply.github.com>
2018-03-27 17:41:04 -0400

    Create imports.rs

---
+++ boot/imports.rs
@@ -0,0 +1,19 @@
+pub fn ionvar() {
+  let ion = "v0.1"
+  println!("starting ion setup...")
+}
+
+pub fn electronvar() {
+  let electron = "v0.0.1"
+  println!("starting electron setup...")
+}
+
+pub fn neutron() {
+  let neutron = "0.1" // honestly, this is currently unavailable
+  println!("starting neutron setup...")
+}
+
+pub fn molecule() {
+  let molecule = "0.0.1"
+  println!("starting molecule setup..."
+}
