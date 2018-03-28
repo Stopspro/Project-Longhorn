2746e858c4dc04b304f18b220e0f91293a7e1ce2
Red Skull <33503692+black-sabbath@users.noreply.github.com>
2018-03-27 20:38:50 -0400

    Update molecule.rs

--- packages/molecule.rs
+++ packages/molecule.rs
@@ -1,5 +1,12 @@
 // in its final form, this should be a GNU Nano editor clone
 pub fn editor() {
-  println!("Molecule Editor - v0.0.1");
-	let mut file = File::create()?; // unfinished, WIP
+    println!("Molecule Editor - v0.0.1");
+    println!("-----------------------------------");
+    let mut file = File::create()?; 
+    let mut line = 
+    let key = 
+    // user input
+    line.push_str(key);
+
+    }	    
 }
