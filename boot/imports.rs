be435a6fa67303e319680764db5a9127cdbf6d20
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-03-27 21:36:33 -0400

    Update imports.rs

--- boot/imports.rs
+++ boot/imports.rs
@@ -1,19 +1,33 @@
-pub fn ionvar() {
-  let ion = "v0.1"
-  println!("starting ion setup...")
-}
+mod pack_mod {
+  pub fn sql_setup() {
+    let ionver =   //sql code call
+    let electronver = 
+    let neutronver = 
+    let moleculever = 
+    let chess = 
+  }
+  
+  pub fn ionvar() {
+    let ion = ionver;
+    println!("starting ion setup...");
+  }
 
-pub fn electronvar() {
-  let electron = "v0.0.1"
-  println!("starting electron setup...")
-}
+  pub fn electronvar() {
+    let electron = electronver; 
+    println!("starting electron setup...");
+  }
 
-pub fn neutron() {
-  let neutron = "0.1" // honestly, this is currently unavailable
-  println!("starting neutron setup...")
-}
+  pub fn neutron() {
+    let neutron = neutronver; // honestly, this is currently unavailable
+    println!("starting neutron setup...");
+  }
 
-pub fn molecule() {
-  let molecule = "0.0.1"
-  println!("starting molecule setup..."
-}
+  pub fn molecule() {
+    let molecule = moleculever;
+    println!("starting molecule setup...");
+  }
+    
+  pub fn chess() {
+    let chess = chessver
+    println!
+  }
