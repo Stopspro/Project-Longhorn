dcce40d002aeec2a81c57a9541c88630d0c3ba94
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-03-30 19:04:08 -0400

    Create chess.rs

---
+++ packages/chess.rs
@@ -0,0 +1,14 @@
+extern crate colored;
+
+fn chess() { // need to implement color code
+  println!("Black or white? 1 for white and 2 for black.");
+  let mut color = 
+  println!("R K B X X B K R")
+  println!("P P P P P P P P")
+  println!("")
+  println!("")
+  println!("")
+  println!("")
+  println!("P P P P P P P P")
+  println!("R K B X X B K R")
+}
