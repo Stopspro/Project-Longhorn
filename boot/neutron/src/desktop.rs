b2a37e8742c31741ad023e073fa8b9720ef6d1b9
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-03-30 13:11:17 -0400

    Create desktop.rs

---
+++ boot/neutron/src/desktop.rs
@@ -0,0 +1,18 @@
+mod desktop {
+  fn load() { // this should pull desktop info from a text file
+  
+  }
+  
+  fn icons() { // this should load icons
+  
+  }
+  
+  fn cursor() { // this should create a cursor
+  
+  }
+  
+  fn terminal() { // this should create a terminal session
+  
+  }
+  
+}
