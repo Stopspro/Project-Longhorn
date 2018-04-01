33e6a8793262c3db08bb879edfe47edc37884b47
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-01 16:43:21 -0400

    Update packages_database.sql

--- sql/packages_database.sql
+++ sql/packages_database.sql
@@ -1,9 +1,5 @@
 CREATE DATABASE packages_database;
 
-CREATE DATABASE packagev_database;
-
-CREATE DATABASE packagecode_database;
-
 USE packages_database;
 CREATE TABLE packages (
   package_id INT NOT NULL,
@@ -21,64 +17,3 @@ VALUES
     (4,"molecule"),
     (5,"chess"),
     (6,"unicorn");
-
-USE packagev_database;
-CREATE TABLE packages (
-  package_id INT NOT NULL,
-  package_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (package_id),
-  UNIQUE (package_name)
-);
-
-INSERT INTO packages;
-    (package_id, package_name) 
-VALUES 
-    (1,"0.1"),
-    (2,"0.0.1"),
-    (3,"0.0.1"),
-    (4,"0.1"),
-    (5,"0.0.1),
-    (6,"0.1");
-     
-USE packagecode_database;
-CREATE TABLE molecule (
-  package_id INT NOT NULL,
-  line_contents VARCHAR(30) NOT NULL,
-  PRIMARY KEY (package_id),
-  UNIQUE (line_contents)
-);
-
-INSERT INTO molecule;
-    (package_id, line_contents)
-VALUES 
-    (1,"lib.rs"),
-    (2,""),
-    (3,"load.rs");
-
-CREATE TABLE chess (
-  package_id INT NOT NULL,
-  line_contents VARCHAR(30) NOT NULL,
-  PRIMARY KEY (package_id),
-  UNIQUE (line_contents)
-);
-
-INSERT INTO chess;
-    (package_id, line_contents) 
-VALUES 
-    (1,"lib.rs"),
-    (2,""),
-    (3,"");
-     
-CREATE TABLE unicorn (
-  package_id INT NOT NULL,
-  line_contents VARCHAR(30) NOT NULL,
-  PRIMARY KEY (package_id),
-  UNIQUE (line_contents)
-);
-
-INSERT INTO unicorn;
-    (package_id, line_contents) 
-VALUES 
-    (1,"lib.rs"),
-    (2,""),
-    (3,"");
