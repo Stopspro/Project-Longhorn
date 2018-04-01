549640e296474a49f01396a9514f37826377e341
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-01 16:54:15 -0400

    Update and rename packages/molecule.rs to packages/molecule/molecule.rs

---
+++ packages/molecule/molecule.rs
@@ -0,0 +1,16 @@
+// in its final form, this should be a GNU Nano editor clone
+#[macro_use]
+extern crate horrorshow; // horrorshow for formatting with HTML
+use horrorshow::prelude::*;
+use horrorshow::doctype;
+
+
+pub fn editor() {
+    let mut file = File::create()?; 
+    let mut line = 
+    let key = 
+    // user input
+    line.push_str(key); // please specify what this does - Hydra
+
+    }	    
+}
