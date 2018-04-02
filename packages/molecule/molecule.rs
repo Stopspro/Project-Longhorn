20baccaa88e7016967ddfb46480096b639ad0d98
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-02 11:58:06 -0400

    Update molecule.rs

--- packages/molecule/molecule.rs
+++ packages/molecule/molecule.rs
@@ -1,4 +1,5 @@
 // in its final form, this should be a GNU Nano editor clone
+
 #[macro_use]
 extern crate horrorshow; // horrorshow for formatting with HTML
 use horrorshow::prelude::*;
@@ -7,10 +8,11 @@ use horrorshow::doctype;
 
 pub fn editor() {
     let mut file = File::create()?; 
-    let mut line = 
-    let key = 
-    // user input
-    line.push_str(key); // please specify what this does - Hydra
-
-    }	    
+    let linenumber = 1; // this is the line that we are on
+    let line = String::from("{}", linenumber); 
+    let key = String::new(); // this is the key that gets added to the string
+    // HTML setup
+    loop {
+           
+    }
 }
