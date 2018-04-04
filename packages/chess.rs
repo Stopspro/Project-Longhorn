1971c5c6fc7d5bac59c27d431f64bae9f04b5b33
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-04 11:34:21 -0400

    Update chess.rs

--- packages/chess.rs
+++ packages/chess.rs
@@ -1,14 +1,16 @@
 #![no_std]
 
+mod vga_buffer;
+
 fn chess() { // need to implement color code
   println!("Black or white? 1 for white and 2 for black.");
   let mut color = 
-  println!("R K B X X B K R")
-  println!("P P P P P P P P")
-  println!("")
-  println!("")
-  println!("")
-  println!("")
-  println!("P P P P P P P P")
-  println!("R K B X X B K R")
+  println!("R K B X X B K R");
+  println!("P P P P P P P P");
+  println!("");
+  println!("");
+  println!("");
+  println!("");
+  println!("P P P P P P P P");
+  println!("R K B X X B K R");
 }
