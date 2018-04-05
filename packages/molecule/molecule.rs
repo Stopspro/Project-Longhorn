26229fef53c258e8a4094dd6beef124211f41005
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-05 17:28:56 -0400

    Update molecule.rs

--- packages/molecule/molecule.rs
+++ packages/molecule/molecule.rs
@@ -1,9 +1,15 @@
 pub fn editor() {
+    println!("                                   ________    _______                      ________ 
+|          |   | | | |   |        |           |         |     |  |         |
+| |      | |   |     |   |        |________   |         |     |  |         |________
+|  |   |   |   |     |   |        |           |         |     |  |         |      
+|    |     |   | | | |   |______  |________   |_______  | | | |  |______   |________  "); // do not edit formatting, it has been tested to work like this
+    
     let mut file = File::create()?; 
     let linenumber = 1; // this is the line that we are on
     let line = String::from("{}", linenumber); 
     let key = String::new(); // this is the key that gets added to the string
     loop {
-           
+            
     }
 }
