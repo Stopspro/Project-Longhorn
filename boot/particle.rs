533f621a8de027636b0ce1af78df4868728492b0
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-05 15:59:10 -0400

    Update particle.rs

--- boot/particle.rs
+++ boot/particle.rs
@@ -1,12 +1,18 @@
 #![no_std]
+
 extern crate postgres;
 
 use postgres::{Connection, SslMode};
+use std::fs::File;
+use std::io::prelude::*;
 
 pub fn download() {
   
 }
 
 pub fn list() {
-  
+  let mut file = File::open("docs.txt")?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+  println!("{}", contents)
 }
