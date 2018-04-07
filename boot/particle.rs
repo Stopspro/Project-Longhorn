14ea9b1283d847147b393b5d4cd701079982c98e
superiorbeing <33503692+superiorbeing@users.noreply.github.com>
2018-04-07 18:35:23 -0400

    Update particle.rs

--- boot/particle.rs
+++ boot/particle.rs
@@ -3,16 +3,11 @@
 extern crate postgres;
 
 use postgres::{Connection, SslMode};
-use std::fs::File;
-use std::io::prelude::*;
 
 pub fn download() {
   
 }
 
 pub fn list() {
-  let mut file = File::open("docs.txt")?;
-  let mut contents = String::new();
-  file.read_to_string(&mut contents)?;
-  println!("{}", contents)
+
 }
