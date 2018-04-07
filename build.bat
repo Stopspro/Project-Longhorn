c66828d77bb4c8af7b75b6216fb5b4bea90dda3c
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-07 09:30:28 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 c:\
 c:\python27\python.exe c:\atomOS\removedocs.py %*
-cd c:\atomOS
+cd c:\atomOS\boot\x86-64
+nasm -f elf64 multiboot_header.asm
+nasm -f elf64 boot.asm
+ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
 pause
