9e7fcb069963204072e47879b44daba6b36cc866
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-07 10:54:01 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,4 +4,10 @@ cd c:\atomOS\boot\x86-64
 nasm -f elf64 multiboot_header.asm
 nasm -f elf64 boot.asm
 ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
-pause
+VBoxManage startvm "Ubuntu" --type headless
+sleep 300
+VBoxManage controlvm "Ubuntu" poweroff --type headless
+VBoxManage startvm "Atom" --type headless
+sleep 300
+VBoxManage controlvm "Ubuntu" poweroff --type headless
+exit /b
