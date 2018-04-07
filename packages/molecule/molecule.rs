4213580e28636923af0051d4d096faadf0259a57
Ozzy Osbourne <33503692+black-sabbath@users.noreply.github.com>
2018-04-07 11:34:05 -0400

    Update molecule.rs

--- packages/molecule/molecule.rs
+++ packages/molecule/molecule.rs
@@ -1,3 +1,7 @@
+extern crate cpuio;
+
+use cpuio::Port;
+
 pub fn editor() {
     println!("                                   ________    _______                      ________ 
 |          |   | | | |   |        |           |         |     |  |         |
@@ -5,11 +9,28 @@ pub fn editor() {
 |  |   |   |   |     |   |        |           |         |     |  |         |      
 |    |     |   | | | |   |______  |________   |_______  | | | |  |______   |________  "); // do not edit formatting, it has been tested to work like this
     
-    let mut file = File::create()?; 
     let linenumber = 1; // this is the line that we are on
     let line = String::from("{}", linenumber); 
     let key = String::new(); // this is the key that gets added to the string
+    let mut keyboard: Port<u8> = unsafe { Port::new(0x60) };
     loop {
+        let command = keyboard.read();
+        let key = keyboard.read();
+        
+        if cmd = 1 { // new file on the block
             
+        }
+        
+        if cmd = 2 { // open file
+            
+        }
+        
+        if cmd = 3 { // save file to block
+            
+        }
+        
+        if cmd = 4 { // exit editor
+            
+        }
     }
 }
