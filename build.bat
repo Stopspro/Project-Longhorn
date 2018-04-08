9e3453c5f05c0a9613533e7500548a6aa62a51de
superiorbeing <33503692+superiorbeing@users.noreply.github.com>
2018-04-08 11:25:47 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,10 +4,5 @@ cd c:\atomOS\boot\x86-64
 nasm -f elf64 multiboot_header.asm
 nasm -f elf64 boot.asm
 ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
-VBoxManage startvm "Ubuntu" --type headless
-sleep 300
-VBoxManage controlvm "Ubuntu" poweroff --type headless
-VBoxManage startvm "Atom" --type headless
-sleep 300
-VBoxManage controlvm "Ubuntu" poweroff --type headless
+cd c:\
 exit /b
