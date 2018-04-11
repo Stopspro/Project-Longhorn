ba5b8d2a17a64f7937846191402f2fe98f864b31
barely-functioning <33503692+barely-functioning@users.noreply.github.com>
2018-04-11 11:19:22 -0400

    Update build.bat

--- z-buildtools/build.bat
+++ z-buildtools/build.bat
@@ -1,6 +1,6 @@
 c:\
-c:\python27\python.exe c:\atomOS\removedocs.py %*
-cd c:\atomOS\boot\x86-64
+c:\python27\python.exe c:\Longhorn\harold.py %*
+cd c:\Longhorn\boot\x86-64
 nasm -f elf64 multiboot_header.asm
 nasm -f elf64 boot.asm
 ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
