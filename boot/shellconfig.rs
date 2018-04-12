fc06cf39f854638e97ad214dfa1f03651f238e07
barely-functioning <33503692+barely-functioning@users.noreply.github.com>
2018-04-12 17:34:51 -0400

    Create shellconfig.rs

---
+++ boot/shellconfig.rs
@@ -0,0 +1,21 @@
+pub fn scryptconfig() {
+  let starter = String::from("~ ");
+  let version = 0.1;
+  let maxchar_norm = 45;
+  let newwin_maxchar = 20;
+  if newwin = 1 {
+    let maxchar_norm = 25;
+  }
+  let conwin_maxchar = 15;
+}
+
+pub fn commsconfig() {
+  let mainip = String::from("IPV6");
+  let altip = String::from("IPV4");
+  let serverport = 8080;
+}
+
+pub fn shellscript() {
+  let commands = String::from("bashmain");
+  let bootup_programs = String::from("none")
+}
