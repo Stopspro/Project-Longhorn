107290c3e0fbbe6233e4e7b621e358705e0b269a
barely-functioning <33503692+barely-functioning@users.noreply.github.com>
2018-04-12 17:01:24 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,5 +4,7 @@ cd c:\Longhorn\boot\x86-64
 nasm -f elf64 multiboot_header.asm
 nasm -f elf64 boot.asm
 ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
-cd c:\
+cd c:\Longhorn
+RUST_TARGET_PATH=$(pwd) xargo build --target=x86_64-longhorn_os
+start /d "c:\Longhorn" start-shell.exe
 exit /b
