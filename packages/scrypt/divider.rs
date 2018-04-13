1a60ede0c7ac5705a4396dd88e4909d3c96efb32
barely-functioning <33503692+barely-functioning@users.noreply.github.com>
2018-04-13 11:55:20 -0400

    Create divider.rs

---
+++ packages/scrypt/divider.rs
@@ -0,0 +1,11 @@
+pub fn newdivider() {
+  if spot = 0 { // default spot
+    loop { 
+      println!("                          |")
+      x = x + 1
+      if x = 25 {
+        break
+      }
+    }
+  }
+}
