ed49d74b82ee9c059a3ea28f86a195a799d05c79
QoupleOfQubits <33503692+QoupleOfQubits@users.noreply.github.com>
2018-04-15 13:25:50 -0400

    Update lib.rs

--- boot/lib.rs
+++ boot/lib.rs
@@ -34,100 +34,150 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     let mut keyboard: Port<u8> = unsafe { Port::new(0x60) };
     let command = String::new();
     let scancode = keyboard.read();
-    if caps = 0 { // because the letter part is different between the two ifs the numbers and characters need to be repeated
-        match scancode {
-	        " " => let letter = "a";
-	        " " => let letter = "b";
-	        " " => let letter = "c";
-	        " " => let letter = "d";
-	        " " => let letter = "e";
-	        " " => let letter = "f";
-	        " " => let letter = "g";
-	        " " => let letter = "h";
-	        " " => let letter = "i";
-	        " " => let letter = "j";
-	        " " => let letter = "k";
-	        " " => let letter = "l";
-	        " " => let letter = "m";
-	        " " => let letter = "n";
-	        " " => let letter = "o";
-	        " " => let letter = "p";
-	        " " => let letter = "q";
-	        " " => let letter = "r";
-	        " " => let letter = "s";
-	        " " => let letter = "t";
-	        " " => let letter = "u";
-	        " " => let letter = "v";
-	        " " => let letter = "w";
-	        " " => let letter = "x";
-	        " " => let letter = "y";
-	        " " => let letter = "z";
-	        // numbers use the letter variable because it is faster
-	        " " => let letter = "0";
-	        " " => let letter = "1";
-	        " " => let letter = "2";
-	        " " => let letter = "3";
-	        " " => let letter = "4";
-	        " " => let letter = "5";
-	        " " => let letter = "6";
-	        " " => let letter = "7";
-	        " " => let letter = "8";
-	        " " => let letter = "9";
-	        // special keys also use the letter variable, see above
-            	" " => let letter = " ";
-		" " => let letter = "!";
-	}
+    loop {
+        if caps = 0 { // because the letter part is different between the two ifs the numbers and characters need to be repeated
+            match scancode {
+    	        " " => let letter = "a"; // make code
+	    	    " " => let letter = "a"; // break code 
+	            " " => let letter = "b"; // repeat
+    		    " " => let letter = "b"; 
+	            " " => let letter = "c";
+    		    " " => let letter = "c";
+	            " " => let letter = "d";
+    		    " " => let letter = "d";
+	            " " => let letter = "e";
+    		    " " => let letter = "e";
+	            " " => let letter = "f";
+	    	    " " => let letter = "f";
+	            " " => let letter = "g";
+	    	    " " => let letter = "g";
+	            " " => let letter = "h";
+	    	    " " => let letter = "h";
+	            " " => let letter = "i";
+	    	    " " => let letter = "i";
+	            " " => let letter = "j";
+	            " " => let letter = "j";
+	            " " => let letter = "k";
+		        " " => let letter = "k";
+	            " " => let letter = "l";
+		        " " => let letter = "l";
+	            " " => let letter = "m";
+		        " " => let letter = "m";
+	            " " => let letter = "n";
+		        " " => let letter = "n";
+	            " " => let letter = "o";
+		        " " => let letter = "o";
+	            " " => let letter = "p";
+		        " " => let letter = "p";
+	            " " => let letter = "q";
+		        " " => let letter = "q";
+	            " " => let letter = "r";
+		        " " => let letter = "r";
+	            " " => let letter = "s";
+		        " " => let letter = "s";
+	            " " => let letter = "t";
+		        " " => let letter = "t";
+	            " " => let letter = "u";
+		        " " => let letter = "u";
+	            " " => let letter = "v";
+		        " " => let letter = "v";
+	            " " => let letter = "w";
+		        " " => let letter = "w";
+	            " " => let letter = "x";
+		        " " => let letter = "x";
+	            " " => let letter = "y";
+		        " " => let letter = "y";
+	            " " => let letter = "z";
+		        " " => let letter = "z";
+	            " " => let letter = "0";
+	            " " => let letter = "1";
+	            " " => let letter = "2";
+	            " " => let letter = "3";
+	            " " => let letter = "4";
+	            " " => let letter = "5";
+	            " " => let letter = "6";
+	            " " => let letter = "7";
+	            " " => let letter = "8";
+	            " " => let letter = "9";
+                " " => let letter = " ";
+		        " " => let letter = "!";
+	    }
 	    
-    if caps = 1 {
-        match scancode {
-	        " " => let letter = "A";
-	        " " => let letter = "B";
-	        " " => let letter = "C";
-	        " " => let letter = "D";
-	        " " => let letter = "E";
-	        " " => let letter = "F";
-	        " " => let letter = "G";
-	        " " => let letter = "H";
-	        " " => let letter = "I";
-	        " " => let letter = "J";
-	        " " => let letter = "K";
-	        " " => let letter = "L";
-	        " " => let letter = "M";
-	        " " => let letter = "N";
-	        " " => let letter = "O";
-	        " " => let letter = "P";
-	        " " => let letter = "Q";
-	        " " => let letter = "R";
-	        " " => let letter = "S";
-	        " " => let letter = "T";
-	        " " => let letter = "U";
-	        " " => let letter = "V";
-	        " " => let letter = "W";
-	        " " => let letter = "X";
-	        " " => let letter = "Y";
-	        " " => let letter = "Z";
-	        // numbers use the letter variable because it is faster
-	        " " => let letter = "0";
-	        " " => let letter = "1";
-	        " " => let letter = "2";
-	        " " => let letter = "3";
-	        " " => let letter = "4";
-	        " " => let letter = "5";
-	        " " => let letter = "6";
-	        " " => let letter = "7";
-	        " " => let letter = "8";
-	        " " => let letter = "9";
-	        // special keys also use the letter variable, see above
-            	" " => let letter = " ";
-		" " => let letter = "!";
-        }
+        if caps = 1 {
+            match scancode {
+                " " => let letter = "a"; // make code
+		        " " => let letter = "a";
+	            " " => let letter = "b";
+		        " " => let letter = "b"; 
+	            " " => let letter = "c";
+		        " " => let letter = "c";
+	            " " => let letter = "d";
+		        " " => let letter = "d";
+	            " " => let letter = "e";
+		        " " => let letter = "e";
+	            " " => let letter = "f";
+		        " " => let letter = "f";
+	            " " => let letter = "g";
+		        " " => let letter = "g";
+	            " " => let letter = "h";
+		        " " => let letter = "h";
+	            " " => let letter = "i";
+		        " " => let letter = "i";
+	            " " => let letter = "j";
+		        " " => let letter = "j";
+	            " " => let letter = "k";
+		        " " => let letter = "k";
+	            " " => let letter = "l";
+		        " " => let letter = "l";
+	            " " => let letter = "m";
+		        " " => let letter = "m";
+	            " " => let letter = "n";
+		        " " => let letter = "n";
+	            " " => let letter = "o";
+		        " " => let letter = "o";
+	            " " => let letter = "p";
+		        " " => let letter = "p";
+	            " " => let letter = "q";
+		        " " => let letter = "q";
+	            " " => let letter = "r";
+		        " " => let letter = "r";
+	            " " => let letter = "s";
+		        " " => let letter = "s";
+	            " " => let letter = "t";
+		        " " => let letter = "t";
+	            " " => let letter = "u";
+		        " " => let letter = "u";
+	            " " => let letter = "v";
+		        " " => let letter = "v";
+	            " " => let letter = "w";
+		        " " => let letter = "w";
+	            " " => let letter = "x";
+		        " " => let letter = "x";
+	            " " => let letter = "y";
+		        " " => let letter = "y";
+	            " " => let letter = "z";
+		        " " => let letter = "z";
+	            " " => let letter = "0";
+	            " " => let letter = "1";
+	            " " => let letter = "2";
+	            " " => let letter = "3";
+	            " " => let letter = "4";
+	            " " => let letter = "5";
+	            " " => let letter = "6";
+	            " " => let letter = "7";
+	            " " => let letter = "8";
+	            " " => let letter = "9";
+                " " => let letter = " ";
+		        " " => let letter = "!";
+            }
+	
+	
+        command.push_str(letter)
 	    
-	command.push_str(letter)
-	    
-	match command {
-		"scrypt" => let command = 1;
-		"comms" => let command = 2;
-	}
+        match command {
+	    "scrypt" => let command = 1;
+	    "comms" => let command = 2;
+	    }  
         
         // bash commands
         if command = 1010 { // shell script_default
@@ -143,16 +193,16 @@ pub extern fn rust_main(multiboot_information_address: usize) {
         }
 
         if command = 1111 { // info
-		println!("Project Longhorn v0.1");
-		println!("A Community-Made OS");
-		println!("------------------------");
-		println!("Developers: ");
-		println!("Hayden Curfman, project leader/ lead programmer");
+            println!("Project Longhorn v0.1");
+		    println!("A Community-Made OS");
+		    println!("------------------------");
+		    println!("Developers: ");
+		    println!("Hayden Curfman, project leader/ lead programmer");
         }
 
         // non bash stuff
         if command = 1 { // scrypt
-		scrypt::editor();
+		    scrypt::editor();
         }
     
         if command = 2 { // comms 
