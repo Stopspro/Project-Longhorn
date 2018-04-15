1c9463dddbe73328c82c6de96b38f234422a77d8
QoupleOfQubits <33503692+QoupleOfQubits@users.noreply.github.com>
2018-04-15 18:14:58 -0400

    Update shellconfig.rs

--- boot/shellconfig.rs
+++ boot/shellconfig.rs
@@ -1,12 +1,13 @@
+// scrypt allows 45 characters in one line, 20 in a new buffer, and 25 in the main bufffer if there is a 2nd buffer open.
 pub fn scryptconfig() {
   let starter = String::from("~ ");
   let version = 0.1;
   let maxchar_norm = 45;
   let newwin_maxchar = 20;
+  let conwin_maxchar = 15;
   if newwin = 1 {
     let maxchar_norm = 25;
   }
-  let conwin_maxchar = 15;
 }
 
 pub fn shellscript() {
