f4e5f14ec79e082ff90c3212a1bb5a435fe489aa
QoupleOfQubits <33503692+QoupleOfQubits@users.noreply.github.com>
2018-04-17 20:30:22 -0400

    Update keybinds.rs

--- packages/scrypt/keybinds.rs
+++ packages/scrypt/keybinds.rs
@@ -3,15 +3,42 @@ extern crate cpuio;
 use cpuio::Port;
 
 pub fn binds() {
-  if visualmode = 1 {
-    if letter = 1 { // A; no bind
-      break
-    }
+    if visualmode = 1 {
+        let letter = String::new();
+        match letter {
+            
+        }
+        
+        if letter = 1 { // A; no bind
+            break
+        }
     
-    if 
-  }
+        if letter = 2 { // A; no bind
+            break
+        }
+        
+        if letter = 3 { // A; no bind
+            break
+        }
+        
+        if letter = 4 { // A; no bind
+            break
+        }
+        
+        if letter = 5 {
+            break
+        }
+        
+        if letter = 6 {
+            break   
+        }
+        
+        if letter = 7 {
+            break   
+        }
+    }
   
-  if visualmode = 0 {
-    println!("Error; visual mode not activated.")
-  }
+    if visualmode = 0 {
+        println!("Error; visual mode not activated.")
+    }
 }
