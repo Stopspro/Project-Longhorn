1f3e8170737536ab679cbb4c4e0fcfa41ba82e6f
QoupleOfQubits <33503692+QoupleOfQubits@users.noreply.github.com>
2018-04-19 11:48:18 -0400

    Update and rename fs.c to disk.c

--- boot/fs.c
+++
@@ -1,5 +0,0 @@
-/* An Example of a Basic Filesystem in C */
-int openDisk(char *filename, int nbytes);
-int readBlock(int disk, int blocknr, void *block);
-int writeBlock(int disk, int blocknr, void *block);
-void syncDisk();
